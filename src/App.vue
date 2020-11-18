60b949cc416e73e72b8258af68978aded473fa73
costel-dev <costelcuconoiu@gmail.com>
2020-11-19 00:37:40 +0100

    added delete button and the functionality to it

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <Todos v-bind:todos="todos" />
+    <Header />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
 // import your componentw that you want to render
+import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
 
 export default {
   name: "App",
   components: {
+    Header,
     Todos,
   },
   data() {
@@ -33,6 +36,11 @@ export default {
         },
       ],
     };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
   },
 };
 </script>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
     <div v-for="todo in todos" v-bind:key="todo.id">
-      <h3>{{ todo.title }}</h3>
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
     </div>
   </div>
 </template>
 
 <script>
+//import your components in order to render them
+import TodoItem from "./TodoItem";
+
 export default {
   name: "Todos",
+  //add your imported file as a component
+  components: {
+    TodoItem,
+  },
   props: ["todos"],
 };
 </script>
